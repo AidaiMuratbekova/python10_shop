733b4cb8919b14382e4202f64624de655e0c1119
Aidai <aidaimuratbekova02@gmail.com>
2021-04-07 21:38:09 +0600

    Add Djangjn project

---
+++ product/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import  Category, Product
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+
+
+#TODO: аЗаполнить товары
+#TODO : елать список товаров на сайте
+#TODO: Добваитть возможность загружать картинки товаров
